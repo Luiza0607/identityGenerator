fc3cea56fdf4281c1ab8f56ec51caf0d3eb1225c
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-02-14 20:23:05 +0100

    add person generator

---
+++ app.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+
+const genders = ['M', 'F'];
+
+const maleNames = [
+	'Jake',
+	'Jack',
+	'Harry',
+	'Jacob',
+	'William',
+	'George',
+	'James',
+	'Ronald',
+	'Connor',
+	'Thomas',
+	'Mike'
+];
+const femaleNames = [
+	'Sophia',
+	'Emily',
+	'Mary',
+	'Linda',
+	'Harriet',
+	'Susan',
+	'Madison',
+	'Rachel',
+	'Lily',
+	'Michelle',
+	'Amanda'
+];
+const lastNames = [
+	'Smith',
+	'Jones',
+	'Williams',
+	'Taylor',
+	'Carter',
+	'Johnson',
+	'Miller.',
+	'Davis',
+	'Brown',
+	'Thompson',
+	'Cruz'
+];
+
+
+function randChoice(arr) {
+	return arr[Math.floor(Math.random() * arr.length)];
+}
+
+const people = [];
+
+for (let i = 0; i < 20; i++) {
+	const gender = randChoice(genders);
+	const firstname = randChoice(gender == 'M' ? maleNames : femaleNames);
+	const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+	const age = Math.floor(Math.random() * (78 - 18 + 1)) + 18;
+	
+	const person = {
+		gender,
+		firstname,
+		lastName,
+		age,
+	};
+
+	people.push(person);
+}
+
+const data = JSON.stringify(people);
+
+fs.writeFile('people.json', data, (err) => {
+	if (err) {
+		console.log('Something went wrong');
+		throw err;
+	}
+	console.log('The file has been saved!');
+});
